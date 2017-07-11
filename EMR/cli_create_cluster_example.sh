b312d03cfcad059161413b4c7802ae03b775939f
Maashu <consulting@understructure.com>
2017-07-11 16:36:33 -0400

    Can't dynamically pass JSON string to ec2 parameter

--- EMR/cli_create_cluster_example.sh
+++ EMR/cli_create_cluster_example.sh
@@ -17,20 +17,23 @@ CLUSTER_NAME=$1
 KEY_NAME=$2
 REGION_NAME=$3
 THE_ENVIRONMENT=$4
+EC2_ATTRIBUTES=\''{"KeyName":"'"$KEY_NAME"'","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-8a2da3a7","EmrManagedSlaveSecurityGroup":"sg-41f36c30","EmrManagedMasterSecurityGroup":"sg-39fa6548","AdditionalMasterSecurityGroups":["sg-fc35a48d"]}'\'
 
-fi
 
+# echo $EC2_ATTRIBUTES
 
 aws emr create-cluster \
 --auto-scaling-role EMR_AutoScaling_DefaultRole \
 --applications Name=Hadoop Name=Hive Name=Zeppelin Name=Spark \
---ec2-attributes '{"KeyName":"${KEY_NAME}","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-8a2da3a7","EmrManagedSlaveSecurityGroup":"sg-41f36c30","EmrManagedMasterSecurityGroup":"sg-39fa6548","AdditionalMasterSecurityGroups":["sg-fc35a48d"]}' \
+--ec2-attributes '{"KeyName":"key-big-data-admin","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-8a2da3a7","EmrManagedSlaveSecurityGroup":"sg-41f36c30","EmrManagedMasterSecurityGroup":"sg-39fa6548","AdditionalMasterSecurityGroups":["sg-fc35a48d"]}' \
 --service-role EMR_DefaultRole \
 --enable-debugging \
 --release-label emr-5.5.0 \
 --log-uri 's3n://muhbutt/emr-logs/' \
---name '${CLUSTER_NAME}' \
+--name "$CLUSTER_NAME" \
 --instance-groups '[{"InstanceCount":1,"BidPrice":"0.05","InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master - 1"}]' \
---configuration file://cluster-config-${THE_ENVIRONMENT}.json \
+--configuration file://cluster-config-$THE_ENVIRONMENT.json \
 --scale-down-behavior TERMINATE_AT_INSTANCE_HOUR \
---region ${REGION_NAME}
+--region "$REGION_NAME"
+
+fi
